db556db6cdf735934b6032894736e006a1f4d989
Stephen Kelly <enda.lee@ittdublin.ie>
2017-03-09 13:05:42 +0000

    Pages and Images Added

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import play.api.Environment;
+import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+
+import views.html.*;
+
+// Import models
+import models.users.*;
+import models.products.*;
+
+
+public class HomeController extends Controller {
+
+    /** Dependency Injection **/
+
+    /** http://stackoverflow.com/questions/15600186/play-framework-dependency-injection **/
+    private FormFactory formFactory;
+
+    /** http://stackoverflow.com/a/37024198 **/
+    private play.api.Environment env;
+
+    /** http://stackoverflow.com/a/10159220/6322856 **/
+    @Inject
+    public  HomeController(Environment e, FormFactory f) {
+        this.env = e;
+        this.formFactory = f;
+    }
+
+    // Get a user - if logged in email will be set in the session
+    @Transactional
+	public User getCurrentUser() {
+		User u = User.getLoggedIn(session().get("email"));
+		return u;
+	}
+
+    @Transactional
+    public Result home() {
+
+        return ok(home.render(User.getLoggedIn(session().get("email"))));
+    }
+
+    @Transactional
+    public Result about() {
+
+        return ok(about.render(User.getLoggedIn(session().get("email"))));
+    }
+
+    @Transactional
+    public Result contact() {
+
+        return ok(contact.render(User.getLoggedIn(session().get("email"))));
+    }
+}
